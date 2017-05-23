d15ceb233fa8c47920f03b13357e765245ba3644
luozhiyong <luozhiyong131@qq.com>
2017-05-23 18:39:00 +0800

    项目建立

---
+++ BusBar/common/common.cpp
@@ -0,0 +1,40 @@
+﻿/*
+ * common.cpp
+ *
+ *  Created on: 2016年10月11日
+ *      Author: Lzy
+ */
+#include "common.h"
+#include <QWidget>
+#include <QHeaderView>
+
+
+/***
+  * 获取程序数据目录
+  */
+QString cm_pathOfData(const QString& name)
+{
+    QDir dataDir = QDir::home();
+    QString dirName = ".Clever";
+    if(!dataDir.exists(dirName))
+        dataDir.mkdir(dirName);
+    dataDir.cd(dirName);
+    return dataDir.absoluteFilePath(name);
+}
+
+
+/***
+  *判断一个字符串是否为纯数字
+  */
+bool cm_isDigitStr(const QString &src)
+{
+    QByteArray ba = src.toLatin1();//QString 转换为 char*
+    const char *s = ba.data();
+
+    while(*s && *s>='0' && *s<='9') s++;
+
+    if (*s) //不是纯数字
+        return false;
+
+    return true;
+}
---
+++ BusBar/common/common.pri
@@ -0,0 +1,18 @@
+
+INCLUDEPATH += $$PWD
+
+HEADERS += \	
+    $$PWD/common.h \
+    $$PWD/msgbox.h \
+#    $$PWD/ipsort.h \
+#    $$PWD/icon/iconpath.h \
+#    $$PWD/signaldispatch.h \
+#    $$PWD/configtool.h
+	
+SOURCES += \	
+    $$PWD/common.cpp \
+    $$PWD/msgbox.cpp \
+#    $$PWD/ipsort.cpp \
+#    $$PWD/icon/iconpath.cpp \
+#    $$PWD/signaldispatch.cpp \
+#    $$PWD/configtool.cpp
