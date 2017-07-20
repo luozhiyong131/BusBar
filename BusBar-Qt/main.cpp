86947bec61c7cadb51672aefbfe9bef49ba66d0f
lzy <luozhiyong131@qq.com>
2017-07-20 11:46:33 +0800

    提交

--- BusBar-Qt/main.cpp
+++ BusBar-Qt/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //  qApp->setFont(QFont("Microsoft YaHei", 10));
+    // qApp->setFont(QFont("Microsoft YaHei", 10));
     //qApp->setFont(QFont("WenQuanYi Micro Hei", 15));
 
     //在main函数中就加载输入法面板,保证整个应用程序可以使用输入法
