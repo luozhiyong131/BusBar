36e8f23c5ea3612a166c07d0393c0c35018ca2a1
lzy <luozhiyong131@qq.com>
2017-07-20 16:29:04 +0800

    11

--- BusBar-Qt/main.cpp
+++ BusBar-Qt/main.cpp
@@ -9,13 +9,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     // qApp->setFont(QFont("Microsoft YaHei", 10));
-    //qApp->setFont(QFont("WenQuanYi Micro Hei", 15));
-
     //在main函数中就加载输入法面板,保证整个应用程序可以使用输入法
-    //以下方法打开中文输入法
-    frmInput::Instance()->Init("control", "black", 11, 11);
-    //以下方法打开数字键盘
-    // frmNum::Instance()->Init("lightgray",10);
+    frmInput::Instance()->Init("control", "black", 11, 11);  //打开中文输入法
+    // frmNum::Instance()->Init("lightgray",10); // 打开数字键盘
 
     MainWindow w;
     w.show();
