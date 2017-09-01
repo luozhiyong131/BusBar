211d688cdb12b27bdb8fefed55290b0c56d2f1f8
lzy <luozhiyong131@qq.com>
2017-09-01 13:41:35 +0800

    关闭功能

--- BusBar-Qt/net/network.cpp
+++ BusBar-Qt/net/network.cpp
@@ -6,11 +6,13 @@ void set_hb_IP(const QStringList &ips);
 
 NetWork::NetWork(QObject *parent) : QObject(parent)
 {
+#if 0
     mNetDataAnalyze = new NetDataAnalyze(this); // 接收IP 模块数据
 
     NetSendThread::bulid()->start(); //平板端连接
 
     QStringList ips;
     ips << "192.168.1.163";
-//    set_hb_IP(ips); // 设置目标IP ，也就是把心跳1包发给住
+    set_hb_IP(ips); // 设置目标IP ，也就是把心跳1包发给住
+#endif
 }
