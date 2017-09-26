b82c7cd6bc6cfd2b048ad99ca8e4bde33e44c1da
lzy <luozhiyong131@qq.com>
2017-09-26 11:50:25 +0800

    机械屏移植

---
+++ CabinetScreen/mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "alarm_thread.h"
+#include "mdb_corethread.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+          
+    void initWindow(void);
+    void connectStatus(void);
+
+signals:
+
+public slots:
+    void timeoutDone(void);
+
+private slots:
+    void on_thresholdBtn_clicked();
+
+    void on_totalBtn_clicked();
+
+    void on_outputBtn_clicked();
+
+    void on_logBtn_clicked();
+
+    void on_timeBtn_clicked();
+
+private:
+    Ui::MainWindow *ui;
+     QTimer *timer;
+
+    MDB_CoreThread *mdb;
+    ALARM_Thread *m_alarm;
+};
+
+#endif // MAINWINDOW_H
