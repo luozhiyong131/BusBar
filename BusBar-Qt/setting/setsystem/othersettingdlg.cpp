70fe4e5fd169e54d1d4e8193f05d1e57ebd84724
lzy <luozhiyong131@qq.com>
2017-10-17 16:23:52 +0800

    升级处理

--- BusBar-Qt/setting/setsystem/othersettingdlg.cpp
+++ BusBar-Qt/setting/setsystem/othersettingdlg.cpp
@@ -15,27 +15,40 @@ OtherSettingDlg::~OtherSettingDlg()
     delete ui;
 }
 
+static bool update_fun(const QString &str)
+{
+    bool ret = true;
+
+    QFileInfo fi(QString("/mnt/%1/busbar/app").arg(str));
+    if(fi.exists()) {
+        int ret = system("rm -rf /mnt/mtdblock3/app");
+        if(ret < 0) {
+            qDebug() << "rm -rf /mnt/mtdblock3/app err ";
+        }
+        QString str = QString("mv /mnt/%1/busbar/app /mnt/mtdblock3/app").arg(str);
+        ret = system(str.toLatin1());
+        if(ret < 0) {
+            qDebug() << str.toLatin1() << " err ";
+        }
+        system("reboot");
+    } else {
+        ret = false;
+    }
+
+    return ret;
+}
 
 void OtherSettingDlg::on_updateBtn_clicked()
 {
     QuMsgBox box(this, tr("是否升级系统?"));
     if(box.Exec()) {
-        QFileInfo fi("/mnt/sda1/busbar/app");                     // 目录存在
-        if(fi.exists()) {
-            int ret = system("rm -rf /mnt/mtdblock3/app");
-            if(ret < 0) {
-                qDebug() << "rm -rf /mnt/mtdblock3/app err ";
-            }
-            ret = system("mv /mnt/sda1/busbar/app /mnt/mtdblock3/app");
-            if(ret < 0) {
-                qDebug() << "mv /mnt/sda1/busbar/app /mnt/mtdblock3/app err ";
-            }
-             system("reboot");
-        } else {
-            CriticalMsgBox box(this, tr("升级文件未找到！\n 请插入U盘，把升级文件放入busbar目录下!"));
-        }
-    }
 
+        bool ret = update_fun("sda1");
+        if(!ret)
+            ret = update_fun("mmcblk0p1");
+        if(!ret)
+            CriticalMsgBox box(this, tr("升级文件未找到！\n 请插入U盘，把升级文件放入busbar目录下!"));
+    }
 }
 
 void OtherSettingDlg::on_resetBtn_clicked()
